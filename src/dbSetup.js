d5954280f007f50c8b9f9f2691393d02fb430877
Marcio Cunha <marcioabcd11@gmail.com>
2022-02-11 15:20:26 -0300

    Preparando Review

--- src/dbSetup.js
+++
@@ -1,8 +0,0 @@
-import {MongoClient} from 'mongodb'
-import dotenv from 'dotenv'
-dotenv.config()
-
-const mongoClient = new MongoClient(process.env.MONGO_URI)
-await mongoClient.connect()
-let db = mongoClient.db("mywallet")
-export default db
